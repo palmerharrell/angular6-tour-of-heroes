522e37301fcdbf77c6b871af73eb1732108899f2
Palmer Harrell <palmerharrell@me.com>
2018-07-12 15:00:23 -0500

    Setting up routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,7 +9,6 @@ import { HeroService } from "../hero.service";
   styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
   heroes: Hero[];
 
   constructor(private heroService: HeroService) {}
@@ -20,9 +19,5 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 }
